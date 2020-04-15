e8e04d02da1d9ad968134a169509014d18f17d5b
fahmichibani <fahmichibani@gmail.com>
2020-04-16 00:31:08 +0100

    fix erreur

--- src/main/java/com/example/classe/Model/Rappel.java
+++ src/main/java/com/example/classe/Model/Rappel.java
@@ -4,43 +4,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data @NoArgsConstructor
 public class Rappel {
 	 @Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	 private int id;
 //	@NotNull
 	    private String titre;
 //	 @NotNull
 	    private String desc;
 //	 @NotNull
-	    private String dater;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitre() {
-		return titre;
-	}
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-	public String getDater() {
-		return dater;
-	}
-	public void setDater(String dater) {
-		this.dater = dater;
-	}
+	    private String datfer;
 	 
 
 }
